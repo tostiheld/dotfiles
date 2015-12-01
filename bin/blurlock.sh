005dccd2f3da7f6475a90c4c5b0b0178785285aa
Thomas Schraven <t.schraven@student.fontys.nl>
2015-12-01 23:42:33 +0100

    better pixel blur size to fit screen resolution

--- bin/blurlock.sh
+++ bin/blurlock.sh
@@ -8,7 +8,7 @@ scrot /tmp/screen.png
 infile=/tmp/screen.png
 outfile=/tmp/screen.png
 # adjust blurriness here
-size=15
+size=16
 
 w=$(convert $infile -format "%[fx:w]" info:)
 h=$(convert $infile -format "%[fx:h]" info:)
