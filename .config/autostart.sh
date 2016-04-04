129dc9ec99fbe4e9c50ccecc24025b66d3147e08
Thomas <t.schraven@student.fontys.nl>
2016-04-04 03:03:57 +0200

    move autostart & make parallel

---
+++ .config/autostart.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# ----------
+# autostart
+# ----------
+
+#compton &
+xsetroot -solid "#555" &
+xset -b &
+xrdb -load .Xresources &
+
+ssh-agent &
+nm-applet &
+pnmixer &
+xfce4-volumed &
+xfce4-power-manager &
+xautolock -time 3 -locker "~/bin/blurlock.sh"  &
+clipit &
+redshift-gtk &
+dropbox start &
+indicator-kdeconnect &
+
