ccc5743774fc3e5b500d8e69573ddf7efe80ee2f
Thomas <t.schraven@student.fontys.nl>
2016-04-04 10:29:24 +0200

    made power menu script better

--- bin/power-menu.sh
+++ bin/power-menu.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 
-rofi_command="rofi -width 10 -hide-scrollbar -bg #586e75 -opacity 100 -padding 5 -dmenu"
+# small power menu using rofi, i3, systemd and pm-utils
+# (last 3 dependencies are adjustable below)
+# tostiheld, 2016
 
-options=$'poweroff\nreboot\nlogout\nhibernate\nsuspend'
+poweroff_command="echo 'systemctl poweroff'"
+reboot_command="systemctl reboot"
+logout_command="i3-msg exit"
+hibernate_command="pm-hibernate"
+suspend_command="pm-suspend"
 
-choice=$(echo "$options" | $rofi_command -p "")
+# you can customise the rofi command all you want ...
+rofi_command="rofi -width 10 -hide-scrollbar -bg #586e75 -opacity 100 -padding 5"
 
-case "$choice" in
-	poweroff)
-		systemctl poweroff
-		;;
-	reboot)
-		systemctl reboot
-		;;
-	logout)
-		i3-msg exit
-		;;
-	hibernate)
-		pm-hibernate
-		;;
-	suspend)
-		pm-suspend
-		;;
-esac
+options=$'poweroff\nreboot\nlogout\nhibernate\nsuspend' 
+
+# ... because the essential options (-dmenu and -p) are added here
+eval \$"$(echo "$options" | $rofi_command -dmenu -p "")_command"
 
