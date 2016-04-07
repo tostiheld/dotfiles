8066a670bda2047f8451552a7e53c4ffd1358559
Thomas <t.schraven@student.fontys.nl>
2016-04-07 18:10:49 +0200

    use systemctl - no sudo

--- bin/power-menu.sh
+++ bin/power-menu.sh
@@ -7,8 +7,8 @@
 poweroff_command="systemctl poweroff"
 reboot_command="systemctl reboot"
 logout_command="i3-msg exit"
-hibernate_command="pm-hibernate"
-suspend_command="pm-suspend"
+hibernate_command="systemctl hibernate"
+suspend_command="systemctl suspend"
 
 # you can customise the rofi command all you want ...
 rofi_command="rofi -width 10 -hide-scrollbar -bg #586e75 -opacity 100 -padding 5"
